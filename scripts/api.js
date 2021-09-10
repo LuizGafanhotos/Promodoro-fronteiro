b208b13456f76a4263f0a7cce4483eef072314f9
LuizGafanhotos <luizfabiano.13lfww@gmail.com>
2021-09-10 12:34:23 -0300

    Eu entendi a primeira, mas a segunda de bosta

--- scripts/api.js
+++ scripts/api.js
@@ -1,6 +1,34 @@
-var number = document.getElementById("number")
-var n = 1;
+// var number = document.getElementById("number").innerText;
+var number = document.getElementsByClassName("big")
+var par = document.getElementsByTagName("p")
+// function upper(){
+//     let conv = parseInt(++number)
+//     par[0].innerText = conv;
+// }
+// function lower(){
+//     let conv = parseInt(--number)
+//     par[0].innerText = conv
+// }
 
-function upper(){
-    number.innerText = `${parseInt(number.text) += n}`
+function bigger_check(){
+    
+
+    if (parseInt(number[0].innerHTML) == 25){
+        let conv = parseInt(++number[0].innerHTML)
+        par[0].innerText = conv
+    }else if (parseInt(number[1].innerHTML) == 5){
+        let conv = parseInt(++number[1].innerHTML)
+        par[1].innerText = conv
+    }else if (parseInt(++number[2].innerHTML) == 3){
+        let conv = ++parseInt(++number[2].innerHTML)
+        par[2].innerText = conv
+    }else{
+        console.log("nope")
+    }
 }
+
+// function diminier_check(){
+//     if (parseInt(number[0].innerHTML) == 25){
+//         let conv = parseInt(++number)
+//     }
+// }
