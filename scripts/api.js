bde2bf727b19f3aff45bb7ab49bb665b15c60456
LuizGafanhotos <luizfabiano.13lfww@gmail.com>
2021-09-11 12:02:23 -0300

    inca doo

--- scripts/api.js
+++ scripts/api.js
@@ -1,25 +1,31 @@
-// var number = document.getElementById("number").innerText;
 var number = document.getElementsByClassName("big")
 var par = document.getElementsByTagName("p")
-// function upper(){
-//     let conv = parseInt(++number)
-//     par[0].innerText = conv;
-// }
-// function lower(){
-//     let conv = parseInt(--number)
-//     par[0].innerText = conv
-// }
 
 function bigger_check(index){
-    let currentNumber = parseInt(number[index].innerText)
-    if (currentNumber == 25){
-        let conv = ++currentNumber
-        par[0].innerText = conv
-    }else if (currentNumber == 5){
-        let conv = ++currentNumber
-        par[2].innerText = conv
-    }else if (currentNumber == 3){
-        let conv = ++currentNumber
-        par[4].innerText = conv
-    }
+    let currentNumber = number[index].innerText
+    console.log(++currentNumber)
+    // if (currentNumber == 25){
+    //     let conv = ++currentNumber
+    //     par[0].innerText = conv
+    // }else if (currentNumber == 5){
+    //     let conv = ++currentNumber
+    //     par[2].innerText = conv
+    // }else if (currentNumber == 3){
+    //     let conv = ++currentNumber
+    //     par[4].innerText = conv
+    // }
+}
+function diminier_check(index){
+    let currentNumber = number[index].innerText 
+    console.log(--currentNumber)
+    // if (currentNumber == 25){
+    //     let conv = --currentNumber
+    //     par[0].innerText = conv
+    // }else if (currentNumber == 5){
+    //     let conv = --currentNumber
+    //     par[2].innerText = conv
+    // }else if (currentNumber == 3){
+    //     let conv = --currentNumber
+    //     par[4].innerText = conv
+    // }
 }
