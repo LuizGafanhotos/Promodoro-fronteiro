0a446d6174932bc9c50f650c106f0a077bccd2ef
LuizGafanhotos <luizfabiano.13lfww@gmail.com>
2021-09-14 14:25:29 -0300

    Agora sim amigo

--- scripts/api.js
+++ scripts/api.js
@@ -2,30 +2,10 @@ var number = document.getElementsByClassName("big")
 var par = document.getElementsByTagName("p")
 
 function bigger_check(index){
-    let currentNumber = number[index].innerText
-    console.log(++currentNumber)
-    // if (currentNumber == 25){
-    //     let conv = ++currentNumber
-    //     par[0].innerText = conv
-    // }else if (currentNumber == 5){
-    //     let conv = ++currentNumber
-    //     par[2].innerText = conv
-    // }else if (currentNumber == 3){
-    //     let conv = ++currentNumber
-    //     par[4].innerText = conv
-    // }
+    const currentNumber = numbers[index];
+    currentNumber.innerText = parseInt(currentNumber.innerText) + 1;
 }
 function diminier_check(index){
-    let currentNumber = number[index].innerText 
-    console.log(--currentNumber)
-    // if (currentNumber == 25){
-    //     let conv = --currentNumber
-    //     par[0].innerText = conv
-    // }else if (currentNumber == 5){
-    //     let conv = --currentNumber
-    //     par[2].innerText = conv
-    // }else if (currentNumber == 3){
-    //     let conv = --currentNumber
-    //     par[4].innerText = conv
-    // }
+    const currentNumber = numbers[index];
+    currentNumber.innerText = parseInt(currentNumber.innerText) - 1; 
 }
